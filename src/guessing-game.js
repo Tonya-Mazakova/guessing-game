40e063f573566f203582e5ed1c88767155923c2b
Tonya-Mazakova <apmazakova@gmail.com>
2017-09-28 19:34:21 +0300

    first

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
-    guess() {
-
+    guess()
+    {
+        this.result = Math.round(this.min + ((this.max - this.min) / 2));
+        return this.result;
     }
-
     lower() {
 
+        this.max = this.result;
+        return this.max;
     }
 
     greater() {
-
+        this.min = this.result;
+        return this.min;
     }
 }
 
